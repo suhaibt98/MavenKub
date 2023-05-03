9dc694aa51b8079b45ee811935bcdb637997e797
Suhaib Telfah <Suhaib.Telfah@progressoft.com>
2023-05-03 14:08:26 +0300

    last attemp

--- update-version.sh
+++ update-version.sh
@@ -4,6 +4,6 @@ set -e
 
 LAST_COMMIT_DATE=$(git log -1 --format=%cd --date=format:'%y%m%d')
 COMMIT_HASH=$(git rev-parse --short=8 HEAD)
-revisionU=$LAST_COMMIT_DATE-$COMMIT_HASH
-echo "$revisionU"
+revision=$LAST_COMMIT_DATE-$COMMIT_HASH
+echo "$revision"
 
