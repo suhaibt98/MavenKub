32c0bb39a0f0cc1323640e5a4f1cd986cbfb5b11
Suhaib Telfah <Suhaib.Telfah@progressoft.com>
2023-05-03 13:30:07 +0300

    Update application version using the maven version plugin and set the new version equals to your last commit date and first eight digits of commit hash~

---
+++ update-version.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+
+LAST_COMMIT_DATE=$(git log -1 --format=%cd --date=format:'%y%m%d')
+COMMIT_HASH=$(git rev-parse --short=8 HEAD)
+revisionU=$LAST_COMMIT_DATE-$COMMIT_HASH
+echo "$revisionU"
+
