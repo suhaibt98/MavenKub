dd2222f21871f9ac15650a48c9aae3936fa087b3
Suhaib Telfah <Suhaib.Telfah@progressoft.com>
2023-05-04 08:58:47 +0300

    back from zero

--- update-version.sh
+++ update-version.sh
@@ -4,6 +4,9 @@ set -e
 
 LAST_COMMIT_DATE=$(git log -1 --format=%cd --date=format:'%y%m%d')
 COMMIT_HASH=$(git rev-parse --short=8 HEAD)
-revision=$LAST_COMMIT_DATE-$COMMIT_HASH
-echo "$revision"
+VERSION="$LAST_COMMIT_DATE-$COMMIT_HASH"
+
+echo "Updating application version to $VERSION"
+
+
 
