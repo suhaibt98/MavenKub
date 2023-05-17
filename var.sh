72068e3c1b77170e53b5b8ba8b7d8078c7bc8c9b
Suhaib Telfah <Suhaib.Telfah@progressoft.com>
2023-05-17 14:19:50 +0300

    k8s by helm

--- var.sh
+++ var.sh
@@ -1,4 +1,4 @@
 export LAST_COMMIT_DATE=$(git log -1 --format=%cd --date=format:'%y%m%d')
 export COMMIT_HASH=$(git rev-parse --short=8 HEAD)
 export  IMAGE_TAG="$LAST_COMMIT_DATE-$COMMIT_HASH"
-   
+echo "$IMAGE_TAG"  
